b8538975281a07c1a55dd039726241adf63505d2
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-02 17:16:56 -0300

    Ruta de Productos

--- clase-01/index.js
+++ clase-01/index.js
@@ -1,34 +1,55 @@
 import * as http from "node:http";
 
+const products = [
+  { id: 1, name: "Laptop", price: 1200 },
+  { id: 2, name: "Mouse", price: 20 },
+];
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
-
-  //   switch (req.url) {
-  //     case "/":
-  //       res.statusCode = 200;
-  //       res.setHeader("Content-Type", "application/json");
-  //       res.end(JSON.stringify({ message: "API Running" }));
-  //       break;
-  //     case "/ping":
-  //       res.statusCode = 200;
-  //       res.setHeader("Content-Type", "application/json");
-  //       res.end(JSON.stringify({ message: "Pong" }));
-  //       break;
-  //   }
 
   if (req.url == "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ message: "API Running" }));
-  } else if (req.url == "/ping") {
+    return;
+  }
+
+  if (req.url == "/ping") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ message: "Pong" }));
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ message: "Not Found" }));
+    return;
   }
+
+  if (req.url == "/products") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(products));
+    return;
+  }
+
+  if (req.url.startsWith("/products/")) {
+    // /products/1
+    const id = Number(req.url.split("/")[2]);
+    const product = products.find((p) => p.id == id);
+
+    if (!product) {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ message: "Producto no encontrado" }));
+      return;
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(product));
+    return;
+  }
+
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ message: "Not Found" }));
 });
 
 server.listen(3000, () => {
