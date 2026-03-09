887e7b255862fa6ed02275993e8e797294c8c9d7
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-09 15:12:37 -0300

    getProductById

---
+++ clase-06/index.js
@@ -0,0 +1,25 @@
+// Solo para el profesor, para evitar problemas de DNS en la conexión a MongoDB Atlas
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import express from "express";
+import mongoose from "mongoose";
+
+import productsRouter from "./routes/products.router.js";
+import categoriesRouter from "./routes/categories.router.js";
+import pingRouter from "./routes/ping.router.js";
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log(error));
+
+app.use(express.json());
+
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+app.use(pingRouter);
+
+app.listen(3000, () => console.log("http://localhost:3000"));
