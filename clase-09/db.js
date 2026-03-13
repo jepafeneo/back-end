7f3c0d9aab824cbf071493a1c3a5b9474e784a66
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-13 15:21:24 -0300

    Config dotenv

--- clase-09/db.js
+++ clase-09/db.js
@@ -1,6 +1,9 @@
 import dns from "node:dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 
 mongoose
--- clase-09/index.js
+++ clase-09/index.js
@@ -1,17 +1,4 @@
-import express from "express";
-import "./db.js";
-
-import productsRouter from "./routes/products.router.js";
-import categoriesRouter from "./routes/categories.router.js";
-import pingRouter from "./routes/ping.router.js";
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/products", productsRouter);
-app.use("/categories", categoriesRouter);
-app.use(pingRouter);
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3001;
 
