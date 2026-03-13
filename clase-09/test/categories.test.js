10897bb0d2d1900bc364fc0a549eea43fca75d00
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-13 17:58:20 -0300

    Test Products/:id y Categories/:id

--- clase-09/test/categories.test.js
+++ clase-09/test/categories.test.js
@@ -24,9 +24,47 @@ describe("Categories endpoint", () => {
       description: "All kinds of books",
     };
     const response = await request(app).post("/categories").send(newCategory);
-    
+
     expect(response.status).to.equal(201);
     expect(response.body).to.have.property("name");
     expect(response.body.name).to.equal("Books");
+  });
+
+  it("Debería traer una categoría por el ID", async function () {
+    const newCategory = {
+      name: "Books",
+      description: "All kinds of books",
+    };
+    const responseCategory = await request(app)
+      .post("/categories")
+      .send(newCategory);
+
+    expect(responseCategory.status).to.equal(201);
+    expect(responseCategory.body).to.have.property("name");
+    expect(responseCategory.body.name).to.equal("Books");
+
+    // Test
+  });
+
+  it("Debería mostrar una categoría por su id", async function () {
+    const newCategory = {
+      name: "Categoría",
+    //   type: "69b44f1436c7b2402be86f5f",
+      description: "Categoría de test",
+    };
+
+    const res = await request(app).post("/categories").send(newCategory);
+
+    expect(res.status).to.equal(201);
+    expect(res.body).to.have.property("name");
+    expect(res.body).to.have.property("description");
+    expect(res.body.name).to.equal("Categoría");
+    expect(res.body.description).to.equal("Categoría de test");
+
+    const response = await request(app).get(`/categories/${res.body._id}`);
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property("name");
+    expect(response.body.name).to.equal("Categoría");
   });
 });
--- clase-09/test/products.test.js
+++ clase-09/test/products.test.js
@@ -3,8 +3,11 @@ import request from "supertest";
 import app from "../app.js";
 
 import Category from "../models/Category.js";
+import Product from "../models/Product.js";
 
 describe("Products endpoint", function () {
+  this.timeout(10000);
+
   it("debería tener un status 200 y un array", async function () {
     const res = await request(app).get("/products");
 
@@ -35,5 +38,46 @@ describe("Products endpoint", function () {
     expect(res.status).to.equal(201);
     expect(res.body).to.have.property("name");
     expect(res.body.name).to.equal("Notebook");
+  });
+
+  it("Debería traer un producto por el id", async function () {
+    // Crear una categoría para asociar al producto
+    const newCategory = {
+      name: "Books",
+      description: "All kinds of books",
+    };
+
+    const responseCategory = await request(app)
+      .post("/categories")
+      .send(newCategory);
+
+    expect(responseCategory.status).to.equal(201);
+    expect(responseCategory.body).to.have.property("name");
+    expect(responseCategory.body.name).to.equal("Books");
+
+    // Crear un producto asociado a la categoría creada
+    const newProduct = {
+      name: "Notebook",
+      price: 1000,
+      stock: 5,
+      category: responseCategory.body._id,
+    };
+
+    const responseProduct = await request(app)
+      .post("/products")
+      .send(newProduct);
+
+    expect(responseProduct.status).to.equal(201);
+    expect(responseProduct.body).to.have.property("name");
+    expect(responseProduct.body.name).to.equal("Notebook");
+
+    // Empieza el test
+    const response = await request(app).get(
+      `/products/${responseProduct.body._id}`,
+    );
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.have.property("name");
+    expect(response.body.name).to.equal("Notebook");
   });
 });
