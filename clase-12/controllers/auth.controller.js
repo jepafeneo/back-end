85ecee1b81aee0bb161e5468aa9d713e51d158ac
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-18 16:42:13 -0300

    Hash Password

--- clase-12/controllers/auth.controller.js
+++ clase-12/controllers/auth.controller.js
@@ -1,3 +1,18 @@
-export const register = (req, res) => {
-  res.send("OK");
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    email,
+    password: hash,
+  });
+
+  res.status(201).json({
+    id: user.id,
+    email: user.email,
+  });
 };
