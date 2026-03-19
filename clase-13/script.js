fb30d3e088483ce947f6133815b72c7885f85a43
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-19 15:25:12 -0300

    Script JWT

--- clase-13/script.js
+++ clase-13/script.js
@@ -1,15 +1,31 @@
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
-const password = "123456";
+// const password = "123456";
 
-const hash1 = await bcrypt.hash(password, 10);
-console.log(hash1);
+// const hash1 = await bcrypt.hash(password, 10);
+// console.log(hash1);
 
-// const hash2 = await bcrypt.hash(password, 10);
-// console.log(hash2);
+// // const hash2 = await bcrypt.hash(password, 10);
+// // console.log(hash2);
 
-const ok = await bcrypt.compare(password, hash1);
-const fail = await bcrypt.compare("1234", hash1);
+// const ok = await bcrypt.compare(password, hash1);
+// const fail = await bcrypt.compare("1234", hash1);
 
-console.log(ok);
-console.log(fail);
+// console.log(ok);
+// console.log(fail);
+
+// ---
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+import { config } from "dotenv";
+config();
+
+import jwt from "jsonwebtoken";
+
+const token = jwt.sign({ userId: 1 }, process.env.JWT_SECRET, {
+  expiresIn: process.env.JWT_EXPIRES_IN,
+});
+
+console.log(token);
